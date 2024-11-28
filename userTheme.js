d0df6abd3b532ad18bbc8ec8283ffa5143d4b9e1
Waiz-ahmed <111419865+Waiz-ahmed@users.noreply.github.com>
2024-11-28 12:14:27 +0500

    Add files via upload

---
+++ userTheme.js
@@ -0,0 +1,11 @@
+{
+    "custom": {
+        "appBar": {
+            "logo": "/site/public/images/arrow-plan.png",
+            "logoHeight": 66,
+            "logoWidth": 200,
+            "background": "#a10207",
+            "activeBackground": "#ffd500"
+        }
+    }
+}
